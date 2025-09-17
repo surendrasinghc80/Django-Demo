0c454fe949f343544dd3a2e29b5a8a70bfdd7426
kaphjgpa <surendrasinghc80@gmail.com>
2025-09-17 23:52:41 +0530

    make changes in the modela and learn abou the the migration and understand dynamic routing in Django

--- chaiaurdjango/chai/models.py
+++ chaiaurdjango/chai/models.py
@@ -13,6 +13,7 @@ class ChaiVarity(models.Model):
     image = models.ImageField(upload_to='chais/')
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+    discription = models.TextField(default="")
 
     def __str__(self):
         return self.name
--- chaiaurdjango/chai/urls.py
+++ chaiaurdjango/chai/urls.py
@@ -5,5 +5,7 @@ from . import views
 #localhost:8000/chai/order
 urlpatterns = [
     path('', views.all_chai , name='all_chai'),
-    # path('order/', views.order , name='order'),
+    path('<int:chai_id>/', views.chai_detail , name='chai_detail'),
+
+
 ]
