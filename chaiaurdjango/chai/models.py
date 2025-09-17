2e5ad4d3d47cda0176a098e0e30a2a62f41d069e
kaphjgpa <surendrasinghc80@gmail.com>
2025-09-17 21:26:40 +0530

    Worked on the model and db config and the images url

--- chaiaurdjango/chai/models.py
+++ chaiaurdjango/chai/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVarity(models.Model):
+    CHAI_TYPE_CHOICES = [
+        ('ML', 'Masala Chai'),
+        ('GT', 'Green Tea'),
+        ('HT', 'Herbal Tea'),
+        ('GR', 'Ginger Tea'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.name
+    
